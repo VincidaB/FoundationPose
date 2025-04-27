Fixed mixed precision in debug mode, but still work in progress

Utils.py
@@ -614,6 +614,12 @@ def compute_crop_window_tf_batch(pts=None, H=None, W=None, poses=None, K=None, c
     tf_precision = torch.float32
   # logging.info("="*30+f" tf_precision set to {tf_precision}")
 
+  if (precision is None or precision < 32) and poses.device == torch.device(type='cpu'):
+    tf_precision_debug = torch.float32
+  else:
+    tf_precision_debug = tf_precision
+  logging.info("="*30+f" tf_precision_debug : {tf_precision_debug}")
+
   B = len(poses)
   # This is a highly malicious way to set it, use with extreme caution
   # torch.set_default_tensor_type('torch.cuda.FloatTensor')
@@ -624,12 +630,12 @@ def compute_crop_window_tf_batch(pts=None, H=None, W=None, poses=None, K=None, c
                         -radius,0,0,
                         0,radius,0,
                         0,-radius,0],
-                        dtype=tf_precision,
+                        dtype=tf_precision_debug,
                         device=poses.device).reshape(-1,3)
     pts = poses[:,:3,3].reshape(-1,1,3)+offsets.reshape(1,-1,3)
-    # logging.info("="*30+f" DEVICES: {poses.device, offsets.device, pts.device}")
-    pts = pts.to(dtype=tf_precision)
-    K = torch.as_tensor(K, dtype=tf_precision, device=poses.device)
+    logging.info("="*30+f" DEVICES: {poses.device, offsets.device, pts.device}")
+    pts = pts.to(dtype=tf_precision_debug)
+    K = torch.as_tensor(K, dtype=tf_precision_debug, device=poses.device)
     # logging.info("="*30+f" TYPE OF K: {K.dtype}, TYPE OF pts: {pts.dtype}")
     projected = (K@pts.reshape(-1,3).T).T
     uvs = projected[:,:2]/projected[:,2:3]
@@ -642,7 +648,7 @@ def compute_crop_window_tf_batch(pts=None, H=None, W=None, poses=None, K=None, c
     top = center[:,1]-radius
     bottom = center[:,1]+radius
     tfs = compute_tf_batch(left, right, top, bottom)
-    return tfs.to(device=poses.device, dtype=tf_precision)
+    return tfs.to(device='cuda', dtype=tf_precision)
 
   else:
     raise RuntimeError

estimater.py
@@ -150,19 +150,23 @@ class FoundationPose:
     logging.info(f"self.rot_grid: {self.rot_grid.shape}")
     logging.info(f"\033[92mmake_rotation_grid time: {time()-start_time:.4f}\033[0m")
 
-  def generate_random_pose_hypo(self, K, rgb, depth, mask, scene_pts=None):
+  def generate_random_pose_hypo(self, K, rgb, depth, mask, scene_pts=None, precision=None):
     '''
     @scene_pts: torch tensor (N,3)
     '''
+    tf_precision = get_tf_precision(precision)
+    logging.info("="*30+f" From hypo, precision is {precision}, tf_precision : {tf_precision}")
     ob_in_cams = self.rot_grid.clone()
-    center = self.guess_translation(depth=depth, mask=mask, K=K)
-    ob_in_cams[:,:3,3] = torch.tensor(center, device='cuda', dtype=torch.float).reshape(1,3)
+    center = self.guess_translation(depth=depth, mask=mask, K=K, precision=precision)
+    ob_in_cams[:,:3,3] = (torch.tensor(center, device='cuda').reshape(1,3))
+    ob_in_cams = ob_in_cams.to(dtype=tf_precision, copy=False)  # For some reason, you can't do it in the previous line
+    logging.info("="*30+f" ob_in_cams dtype : {ob_in_cams.dtype}")
     return ob_in_cams
 
 
   def guess_translation(self, depth, mask, K, precision=None):
     np_precision = get_np_precision(precision)
-    # print("="*30+f" K IS OF DTYPE: {type(K)} ")
+    print("="*30+f" K IS OF DTYPE: {type(K)}, precision is {precision} ")
     vs,us = np.where(mask>0)
     if len(us)==0:
       logging.info(f'mask is all zero')
@@ -175,16 +179,18 @@ class FoundationPose:
       return np.zeros((3))
 
     zc = np.median(depth[valid])
-    if precision is None or precision < 32:
+    if precision is None or precision < 32:  # linalg doesn't accept precision 16
       K_copy = K.astype(dtype=np.float32)
     else:
       K_copy = K
-    center = (np.linalg.inv(K_copy)@np.asarray([uc,vc,1]).reshape(3,1))*zc
+    center = (np.linalg.inv(K_copy)@np.asarray([uc,vc,1]).reshape(3,1)).astype(dtype=np_precision)*zc
+    logging.info("="*30+f" center dtype: {center.dtype}")
 
     if self.debug>=2:
       pcd = toOpen3dCloud(center.reshape(1,3))
       o3d.io.write_point_cloud(f'{self.debug_dir}/init_center.ply', pcd)
     logging.info(f'guess_translation: {center.reshape(3)}')
+    logging.info("="*30+f" center dtype: {center.dtype}")
     return center.reshape(3)
 
 
@@ -194,6 +200,7 @@ class FoundationPose:
     '''
     set_seed(0)
     logging.info('Welcome')
+    logging.info("="*30+f" Registering with precision {precision}")
     
     np_precision = get_np_precision(precision)
 
@@ -206,7 +213,7 @@ class FoundationPose:
 
     #depth = erode_depth(depth, radius=2, device='cuda')
     #depth = bilateral_filter_depth(depth, radius=2, device='cuda')
-    logging.info("="*30+f" DEPTH DTYPE: {depth.dtype}")
+    # logging.info("="*30+f" DEPTH DTYPE: {depth.dtype}")
     
     if self.debug>=2:
       xyz_map = depth2xyzmap(depth, K, precision=precision)
@@ -214,6 +221,7 @@ class FoundationPose:
       pcd = toOpen3dCloud(xyz_map[valid], rgb[valid])
       o3d.io.write_point_cloud(f'{self.debug_dir}/scene_raw.ply',pcd)
       cv2.imwrite(f'{self.debug_dir}/ob_mask.png', (ob_mask*255.0).clip(0,255))
+      logging.info("="*30+f" xyz_map : {xyz_map.dtype}")
 
     normal_map = None
     valid = (depth>=0.001) & (ob_mask>0)
@@ -229,6 +237,7 @@ class FoundationPose:
       valid = xyz_map[...,2]>=0.001
       pcd = toOpen3dCloud(xyz_map[valid], rgb[valid])
       o3d.io.write_point_cloud(f'{self.debug_dir}/scene_complete.ply',pcd)
+      logging.info("="*30+f" pcd : {type(pcd)}")
 
     self.H, self.W = depth.shape[:2]
     K = K.astype(dtype=np_precision, copy=False)
@@ -237,7 +246,9 @@ class FoundationPose:
     self.ob_mask = ob_mask
 
     start_time_poses = time()
-    poses = self.generate_random_pose_hypo(K=K, rgb=rgb, depth=depth, mask=ob_mask, scene_pts=None)
+    logging.info("="*30+f" K : {K.dtype}, rgb : {rgb.dtype}, depth : {depth.dtype}")
+    poses = self.generate_random_pose_hypo(K=K, rgb=rgb, depth=depth, mask=ob_mask, scene_pts=None, precision=precision)
+    # logging.info("="*30+f" poses dtype: {poses.dtype}")
     poses = poses.data.cpu().numpy()
     # logging.info("="*30+f" poses dtype: {poses.dtype}")
     logging.info(f'poses:{poses.shape}')
@@ -245,7 +256,7 @@ class FoundationPose:
 
 
     start_time_guess_translation = time()
-    center = self.guess_translation(depth=depth, mask=ob_mask, K=K)
+    center = self.guess_translation(depth=depth, mask=ob_mask, K=K, precision=precision)
     logging.info(f"\033[92mguess_translation time: {time()-start_time_guess_translation:.4f}\033[0m")
 
 
@@ -253,11 +264,11 @@ class FoundationPose:
     poses[:,:3,3] = torch.as_tensor(center.reshape(1,3), device='cuda')
 
     start_add_errs = time()
-    add_errs = self.compute_add_err_to_gt_pose(poses)
+    add_errs = self.compute_add_err_to_gt_pose(poses, precision=precision)
     logging.info(f"after viewpoint, add_errs min:{add_errs.min()}")
     logging.info(f"\033[92mcompute_add_err_to_gt_pose time: {time()-start_add_errs:.4f}\033[0m")
 
-    xyz_map = depth2xyzmap(depth, K)
+    xyz_map = depth2xyzmap(depth, K, precision=precision)
     start_time_scorer = time()
     poses, vis = self.refiner.predict(mesh=self.mesh, mesh_tensors=self.mesh_tensors, rgb=rgb, depth=depth, K=K, ob_in_cams=poses.data.cpu().numpy(), normal_map=normal_map, xyz_map=xyz_map, glctx=self.glctx, mesh_diameter=self.diameter, iteration=iteration, get_vis=self.debug>=2, precision=precision)
     if vis is not None:
@@ -272,7 +283,7 @@ class FoundationPose:
       imageio.imwrite(f'{self.debug_dir}/vis_score.png', vis)
     logging.info(f"\033[92ms refiner score time: {time()-start_time_scorer:.4f}\033[0m")
 
-    add_errs = self.compute_add_err_to_gt_pose(poses)
+    add_errs = self.compute_add_err_to_gt_pose(poses, precision=precision)
     logging.info(f"final, add_errs min:{add_errs.min()}")
 
     ids = torch.as_tensor(scores).argsort(descending=True)
@@ -292,11 +303,11 @@ class FoundationPose:
     return best_pose.data.cpu().numpy()
 
 
-  def compute_add_err_to_gt_pose(self, poses):
+  def compute_add_err_to_gt_pose(self, poses, precision=None):
     '''
     @poses: wrt. the centered mesh
     '''
-    return -torch.ones(len(poses), device='cuda', dtype=torch.float)
+    return -torch.ones(len(poses), device='cuda', dtype=get_tf_precision(precision))
 
 
   def track_one(self, rgb, depth, K, iteration, extra={}):

learning/training/predict_pose_refine.py
@@ -29,8 +29,9 @@ def make_crop_data_batch(render_size, ob_in_cams, mesh, rgb, depth, K, crop_rati
   H,W = depth.shape[:2]
   args = []
   method = 'box_3d'
-  tf_to_crops = compute_crop_window_tf_batch(pts=mesh.vertices, H=H, W=W, poses=ob_in_cams, K=K, crop_ratio=crop_ratio, out_size=(render_size[1], render_size[0]), method=method, mesh_diameter=mesh_diameter, precision=precision)
   tf_precision = get_tf_precision(precision)
+  logging.info("="*30+f" Devices for ob_in_cams : {ob_in_cams.device}, K : {type(K)}")
+  tf_to_crops = compute_crop_window_tf_batch(pts=mesh.vertices, H=H, W=W, poses=ob_in_cams, K=K, crop_ratio=crop_ratio, out_size=(render_size[1], render_size[0]), method=method, mesh_diameter=mesh_diameter, precision=precision)
 
   logging.info("make tf_to_crops done")
 
@@ -283,7 +284,8 @@ class PoseRefinePredictor:
       logging.info("get_vis...")
       canvas = []
       padding = 2
-      pose_data = make_crop_data_batch(self.cfg.input_resize, torch.as_tensor(ob_centered_in_cams), mesh_centered, rgb, depth, K, crop_ratio=crop_ratio, normal_map=normal_map, xyz_map=xyz_map_tensor, cfg=self.cfg, glctx=glctx, mesh_tensors=mesh_tensors, dataset=self.dataset, mesh_diameter=mesh_diameter, precision=precision)
+      # logging.info("="*30+f" Devices for K {K.device}, ")
+      pose_data = make_crop_data_batch(self.cfg.input_resize, torch.as_tensor(ob_centered_in_cams, dtype=tf_precision), mesh_centered, rgb, depth, K, crop_ratio=crop_ratio, normal_map=normal_map, xyz_map=xyz_map_tensor, cfg=self.cfg, glctx=glctx, mesh_tensors=mesh_tensors, dataset=self.dataset, mesh_diameter=mesh_diameter, precision=precision)
       for id in range(0, len(B_in_cams)):
         rgbA_vis = (pose_data.rgbAs[id]*255).permute(1,2,0).data.cpu().numpy()
         rgbB_vis = (pose_data.rgbBs[id]*255).permute(1,2,0).data.cpu().numpy()
@@ -307,7 +309,7 @@ class PoseRefinePredictor:
         canvas.append(row)
       canvas = make_grid_image(canvas, nrow=1, padding=padding, pad_value=255)
 
-      pose_data = make_crop_data_batch(self.cfg.input_resize, B_in_cams, mesh_centered, rgb, depth, K, crop_ratio=crop_ratio, normal_map=normal_map, xyz_map=xyz_map_tensor, cfg=self.cfg, glctx=glctx, mesh_tensors=mesh_tensors, dataset=self.dataset, mesh_diameter=mesh_diameter)
+      pose_data = make_crop_data_batch(self.cfg.input_resize, B_in_cams, mesh_centered, rgb, depth, K, crop_ratio=crop_ratio, normal_map=normal_map, xyz_map=xyz_map_tensor, cfg=self.cfg, glctx=glctx, mesh_tensors=mesh_tensors, dataset=self.dataset, mesh_diameter=mesh_diameter, precision=precision)
       canvas_refined = []
       for id in range(0, len(B_in_cams)):
         rgbA_vis = (pose_data.rgbAs[id]*255).permute(1,2,0).data.cpu().numpy()

run_ipd.py
@@ -28,16 +28,7 @@ if __name__=='__main__':
   set_logging_format(level=logging.INFO)
   set_seed(0)
 
-  if args.precision == 32:
-    tf_precision = torch.float32
-    np_precision = np.float32
-  elif args.precision == 16:
-    tf_precision = torch.float16
-    np_precision = np.float16
-  elif args.precision == 64:
-    tf_precision = torch.float64
-    np_precision = np.float64
-  else:
+  if args.precision not in [64, 32, 16]:
     raise ValueError(f"Precision must be 64, 32 or 16 (default: 32). Your value: {args.precision}.")
 
   mesh = trimesh.load(args.mesh_file)
@@ -64,7 +55,7 @@ if __name__=='__main__':
     color = reader.get_color(i)
     depth = reader.get_depth(i)
     # scale depth by a factor of 0.1
-    depth = (depth*0.1).astype(np_precision)
+    depth = (depth*0.1).astype(get_np_precision(args.precision))
     if i==0:
       mask = reader.get_mask(0).astype(bool)
       start_time = time()